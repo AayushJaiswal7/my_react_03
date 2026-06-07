12e0f44da443d8663158984265b51bc06b676d96
Aayush Kumar <aayushkr799@gmail.com>
2026-06-07 23:02:58 +0530

    feat: add Form component with controlled state for full name and username inputs

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,32 +1,46 @@
 import { useState } from "react";
 
 export default function Form() {
-   // let [name, setName] = useState("");
-   // let [username,setUsername]=useState("");
    let [formData,setFromData]=useState({
       fullName:"",
-      userName:"",
+      username:"",
    })
-   let handleInputChange=()=>{
-      
+   let handleInputChange=(event)=>{
+      let fieldName=event.target.name;
+      let newValue=event.target.value;
+      setFromData((currData)=>{
+         currData[fieldName]=newValue
+         return{...currData};
+      })    
    }
+   let handleFormSubmit = (event)=>{
+      event.preventDefault();
+      setFromData({
+         fullName:"",
+         username:"",
+      })
+
+   }
+      
    return (
       <>
-         <form style={{ marginTop: "250px" }} >
+         <form onSubmit={handleFormSubmit} style={{ marginTop: "250px" }} >
             <label htmlFor="fullName" style={{marginRight:"50px"}}>Full Name</label>
             <input
                placeholder="enter your full name" 
                type="text"
                value={formData.fullName}
                id="fullName"
-               onChange={handle} />
+               name="fullName"
+               onChange={handleInputChange} />
             <label htmlFor="username" style={{marginRight:"50px"}}>Full Username</label>
             <input
                placeholder="enter your username" 
                type="text"
                value={formData.username}
                id="username"
-               onChange={(e)=>setUsername(e.target.value)} />
+               name="username"
+               onChange={handleInputChange} />
             <button style={{marginLeft:"50px"}}>Submit</button>
          </form>
       </>
